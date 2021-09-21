235e5e649556e143019af2908bb9d0338115d8ea
Carlos Henrique Lima Cicoti <Carlosmvsep3@hotmail.com>
2021-09-21 15:02:11 -0300

    Criando a classe PessoaContext e colocando a string de conexao

---
+++ Models/PessoaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroDeCliente.Models
+{
+    public class PessoaContext : DbContext
+    {
+        public PessoaContext(DbContextOptions<PessoaContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<PessoaContext> pessoas { get; set; }
+
+
+    }
+}
