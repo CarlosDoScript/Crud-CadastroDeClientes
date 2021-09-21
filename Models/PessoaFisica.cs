fcf7affe91b3edb7f914d6c5640463f6b0c00dc9
Carlos Henrique Lima Cicoti <Carlosmvsep3@hotmail.com>
2021-09-21 15:37:54 -0300

    Colocando nome e documento pessoas como obrigatorios

--- Models/PessoaFisica.cs
+++ Models/PessoaFisica.cs
@@ -9,18 +9,39 @@ namespace CadastroDeCliente.Models
     public class PessoaFisica
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = " O tamanho de {0} deve estar entre {2} e {1}")]
         public String Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         public long Cpf { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         public long Rg { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         public long Telefone { get; set; }
+
         [Display(Name = "Data de nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DtNascimento { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = " O tamanho de {0} deve estar entre {2} e {1}")]
         public string Rua { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Display(Name = "Número")]
         public int NumeroCasa { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = " O tamanho de {0} deve estar entre {2} e {1}")]
         public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = " O tamanho de {0} deve estar entre {2} e {1}")]
         public string Cidade { get; set; }
     }
 }
