4efbd8f45affea08d77aad9883746f6bbdd5a3c7
Carlos Henrique Lima Cicoti <Carlosmvsep3@hotmail.com>
2021-09-21 14:50:37 -0300

    Adicionando a classe PessoaFisica

---
+++ Models/PessoaFisica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDeCliente.Models
+{
+    public class PessoaFisica
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public long Cpf { get; set; }
+        public long Rg { get; set; }
+        public long Telefone { get; set; }
+        public DateTime DtNascimento { get; set; }
+        public string Rua { get; set; }
+        public int NumeroCasa { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+    }
+}
